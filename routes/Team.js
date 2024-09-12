9e970495eebe07917c129f0e9dddfcf8f6b8c4a2
ra72e6 <ra72e6@hanmail.net>
2024-09-12 16:21:59 +0900

    09122st

--- routes/Team.js
+++ routes/Team.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const path = require('path');
 
 router.get('/', function (req, res) {
-  res.render('/team.ejs');
+  res.render('team.ejs');
 });
 
 module.exports = router;
